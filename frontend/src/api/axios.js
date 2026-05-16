586f9ecd4848bf3333ee401e770a8a8c3acb72dc
Sanju <sanju85jaat55@gmail.com>
2026-05-16 21:42:18 +0530

    Updated frontend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-production-f5b1.up.railway.app/api",
 });
 
 API.interceptors.request.use((config) => {

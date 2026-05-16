8d3154c61b9f72775b936e4ed043be3aa20b240e
Sanju <sanju85jaat55@gmail.com>
2026-05-16 21:59:47 +0530

    Fixed Railway frontend host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
+iimport { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["valiant-encouragement-production-3596.up.railway.app"],
+  },
 });
